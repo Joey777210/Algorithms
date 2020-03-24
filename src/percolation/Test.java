5a71e7b49bf73dea8401fc498b3ffd48e7e24fb2
Joey777210 <Joeyzq970519@163.com>
2020-03-24 16:32:54 +0800

    first commit,week1

---
+++ src/percolation/Test.java
@@ -0,0 +1,13 @@
+package percolation;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+public class Test {
+    public static void main(String[] args){
+        while(true){
+            int x = StdRandom.uniform(5);
+            System.out.println(x);
+        }
+
+    }
+}
